d837c5b2fade6f8cc4356a918d94ce164ff31e05
kingy <kingychiu@gmail.com>
2017-03-06 20:54:00 +0800

    save

--- spark_cnn_1.py
+++ spark_cnn_1.py
@@ -94,7 +94,6 @@ for i in range(0, 100):
     print('#############################')
     stat_lines.append(str((i + 1) * 10) + ': ' + str(score1[1]) + ', ' + str(score2[1]))
     FileIO.write_lines_to_file('./cnn_1.log', stat_lines)
-    if i % 10 == 0 and i != 0:
-        model.save('./models/cnn_1_' + str((i + 1) * 10) + 'ep.h5')
+    model.save('./models/cnn_1_' + str((i + 1) * 10) + 'ep.h5')
 sc.stop()
 ## END OF SPARK ##
