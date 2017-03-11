4a51800378e628ad096c9b55426bf85ac152738b
kingy <kingychiu@gmail.com>
2017-03-12 07:34:12 +0800

    config

--- spark_cnn_5.py
+++ spark_cnn_5.py
@@ -39,23 +39,29 @@ pool_size = (1, 2)
 model = Sequential()
 
 # Convolution Layer(s)
-model.add(Convolution2D(128, 3, 1,
+model.add(Convolution2D(16, 3, 1,
                         border_mode="same",
                         # (channel, row, col)
                         input_shape=(1, dimension, 1)))
 model.add(Activation('relu'))
-model.add(Convolution2D(128, 3, 1, border_mode='same'))
+model.add(Convolution2D(16, 3, 1, border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=pool_size))
 print(model.output_shape)
 
-model.add(Convolution2D(128, 3, 1, border_mode="same"))
+model.add(Convolution2D(32, 3, 1, border_mode="same"))
 model.add(Activation('relu'))
-model.add(Convolution2D(128, 3, 1, border_mode='same'))
+model.add(Convolution2D(32, 3, 1, border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=pool_size))
 print(model.output_shape)
 
+model.add(Convolution2D(64, 3, 1, border_mode="same"))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 1, border_mode='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+print(model.output_shape)
 
 # Fully Connected Layer
 model.add(Flatten())
