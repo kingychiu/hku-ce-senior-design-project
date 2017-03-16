8bdab61e14ee7b6e968f1bf51b2d0b5a0ea31fe5
kingy <kingychiu@gmail.com>
2017-03-17 00:13:43 +0800

    more filters

--- gpu_cnn_look_up.py
+++ gpu_cnn_look_up.py
@@ -121,8 +121,8 @@ for i in range(0, 100):
     lines.append(','.join([str(a) for a in loss]))
     lines.append(','.join([str(a) for a in acc]))
     lines.append(','.join([str(a) for a in val_acc]))
-    FileIO.write_lines_to_file('./gpu_look_up_cnn_' + str(num_conv_block) + '_convB_6_layers.log',
+    FileIO.write_lines_to_file('./gpu_look_up_cnn_' + str(num_conv_block) + '_convB_4_layers.log',
                                lines)
     model.save(
         './models/gpu_look_up_cnn_epoch_' + str((i + 1) * epoch_step) + 'ep_' + str(
-            num_conv_block) + '_convB_6_layers.h5')
+            num_conv_block) + '_convB_4_layers.h5')
