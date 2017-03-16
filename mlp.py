f1f532764db1b4d127579f53eec02a8b5af38113
kingy <kingychiu@gmail.com>
2017-03-16 17:10:38 +0800

    lookupcnn

--- mlp.py
+++ mlp.py
@@ -48,7 +48,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
-                    batch_size=batch_size, epochs=epochs,
+                    batch_size=batch_size, nb_epoch=epochs,
                     verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
