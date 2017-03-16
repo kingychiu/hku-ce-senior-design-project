be4e869d03c7b05f2ef2ea03ca4c53c8cb08809b
kingy <kingychiu@gmail.com>
2017-03-16 17:10:02 +0800

    lookupcnn

--- gpu_cnn_look_up.py
+++ gpu_cnn_look_up.py
@@ -108,7 +108,7 @@ acc = []
 val_acc = []
 start_time = datetime.datetime.now()
 for i in range(0, 20):
-    history = model.fit(x_train, y_train, 64, epoch_step,
+    history = model.fit(x_train, y_train, 128, epoch_step,
                         verbose=1, validation_data=(x_test, y_test))
 end_time = datetime.datetime.now()
 print(str(end_time - start_time))
