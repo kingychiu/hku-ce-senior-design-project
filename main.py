86ab2527e91683005680b7454ccb2ba6526fc3ef
kingy <kingychiu@gmail.com>
2017-03-18 15:44:24 +0800

    preprocess

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from preprocess import PreProcess
 p = PreProcess('./datasets/ag_dataset_gt_50.txt')
 labels, tensor = p.run_look_up()
 print(tensor[0])
-with open('./datasets/ag_7blkup_5_cl_gt_50.txt', 'w', encoding='utf8') as f1:
+with open('./datasets/ag_7blkup_4_cl_gt_50.txt', 'w', encoding='utf8') as f1:
     document_strs = []
     for matrix in tensor:
         char_strs = []
@@ -13,7 +13,7 @@ with open('./datasets/ag_7blkup_5_cl_gt_50.txt', 'w', encoding='utf8') as f1:
         document_strs.append(document_str)
     lines = []
     for i in range(len(document_strs)):
-        # if labels[i] != 'Entertainment':
-        lines.append(labels[i] + '|l|' + document_strs[i])
+        if labels[i] != 'Entertainment':
+            lines.append(labels[i] + '|l|' + document_strs[i])
 
     f1.write('\n'.join(lines))
