499aeca7a289fdd2d82ebb58ed54df35b2c9cdc0
kingy <kingychiu@gmail.com>
2017-03-19 00:56:56 +0800

    knn

--- knn.py
+++ knn.py
@@ -5,13 +5,14 @@ from sklearn.cluster import KMeans
 with open('./datasets/7blkup_4classes_dfeatures.txt', 'r', encoding='utf8') as f:
     lines = f.readlines()
     labels = []
+    classes = sorted(list(set(labels)))
     features = []
     for line in lines:
         labels.append(line.split('|sep|')[0])
         features.append(line.split('|sep|')[1].split(','))
     print(labels[:10])
     print(len(features[0]))
-x_train, x_test, y_train, y_test = train_test_split(features[:2000], labels[:2000], test_size=0.3,
+x_train, x_test, y_train, y_test = train_test_split(features[:1000], labels[:1000], test_size=0.3,
                                                     random_state=42)
 print(len(x_train))
 print(len(x_train[0]))
@@ -31,9 +32,15 @@ for sample in x_test:
 print(predictions[:10])
 
 t = 0
+t_by_class = {}
+for c in classes:
+    t_by_class[c] = 0
+
 for i in range(len(predictions)):
     p = predictions[i][0]
     if p == y_test[i]:
-        t = t + 1
+        t += 1
+        t_by_class[p] += 1
 
 print(t / len(predictions))
+print(t_by_class)
