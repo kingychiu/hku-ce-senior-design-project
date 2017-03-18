232634811bf95d3739bbefe26192fed8e69734c1
kingy <kingychiu@gmail.com>
2017-03-19 00:19:35 +0800

    knn

--- knn.py
+++ knn.py
@@ -11,23 +11,19 @@ with open('./datasets/7blkup_4classes_dfeatures.txt', 'r', encoding='utf8') as f
         features.append(line.split('|sep|')[1].split(','))
     print(labels[:10])
     print(len(features[0]))
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.5,
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.7,
                                                     random_state=42)
 print(len(x_train))
 print(len(x_train[0]))
 print(len(x_test))
 del labels
 del features
-import sys
 
-print(sys.getsizeof(x_train))
-# kmeans = KMeans(n_clusters=5, random_state=0).fit(x_train)
-# neigh = KNeighborsClassifier(n_neighbors=3, algorithm='brute')
-# neigh.fit(x_train, y_train)
-#
-# predictions = []
-# for x in x_test:
-#     predictions.append(neigh.predict(x))
-# print(predictions[:10])
+neigh = KNeighborsClassifier(n_neighbors=3, algorithm='brute')
+neigh.fit(x_train, y_train)
+
+predictions = []
+for x in x_test:
+    predictions.append(neigh.predict(x))
+print(predictions[:10])
 # for i in range(len(predictions)):
-#
