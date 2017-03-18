f4ea8d57403209c1ce7dd9b4505ecee102312ca8
kingy <kingychiu@gmail.com>
2017-03-18 11:57:02 +0800

    read model

--- read_model_7bits.py
+++ read_model_7bits.py
@@ -1,19 +1,10 @@
 # numpy
 import numpy as np
 # keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
-from keras.optimizers import SGD, Adam, Adadelta
 # sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-# spark, elephas
-from keras.optimizers import SGD, Adam
-# other
-from file_io import FileIO
-import datetime
 
 
 def get_data():
@@ -42,21 +33,17 @@ def get_data():
         print('Labels', classes)
 
         # shuffle
-        x, y = shuffle(x, y, random_state=0)
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
         f.close()
-        return x_train, x_test, y_train, y_test, len(classes)
+        return x, y, len(classes)
 
 
-x_train, x_test, y_train, y_test, num_classes = get_data()
+x, y, num_classes = get_data()
 # Convert class vectors to binary class matrices
-y_train = np_utils.to_categorical(y_train, nb_classes)
-y_test = np_utils.to_categorical(y_test, nb_classes)
+y = np_utils.to_categorical(y, num_classes)
 
 from keras.models import load_model
+
 model_path = './models/gpu_look_up_cnn_epoch_60ep_3_convB_4_layers.h5'
 model = load_model(model_path)
-score, acc = model.evaluate(x_train, y_train, verbose=0)
-print('Train accuracy:', acc)
-score, acc = model.evaluate(x_test, y_test, verbose=0)
-print('Test accuracy:', acc)
+score, acc = model.evaluate(x, y, verbose=0)
+print('Whole accuracy:', acc)
