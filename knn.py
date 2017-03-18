1e93a067331f9a1d6b9907efafbd308b305e3d14
kingy <kingychiu@gmail.com>
2017-03-18 23:45:08 +0800

    knn

--- knn.py
+++ knn.py
@@ -10,6 +10,16 @@ with open('./datasets/7blkup_4classes_dfeatures.txt', 'r', encoding='utf8') as f
         features.append(line.split('|sep|')[1].split(','))
     print(labels[:10])
     print(len(features[0]))
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
-print(x_train.shape)
-print(x_test.shape)
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3,
+                                                    random_state=42)
+print(len(x_train))
+print(len(x_test))
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(x_train, y_train)
+
+predictions = []
+for x in x_test:
+    predictions.append(neigh.predict(x))
+print(predictions[:10])
+# for i in range(len(predictions)):
+#
