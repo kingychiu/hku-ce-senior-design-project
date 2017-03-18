46cbaee5f36a7651deae44bd9bd8e0cc8768f756
kingy <kingychiu@gmail.com>
2017-03-18 21:36:19 +0800

    preprocess

--- clustering.py
+++ clustering.py
@@ -9,6 +9,38 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+def get_data():
+    with open('./datasets/ag_7blkup_4_cl_gt_50.txt', 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        tensor = []
+        labels = []
+        print(len(lines))
+        for line in lines:
+            matrix = []
+            labels.append(line.split('|l|')[0])
+            char_look_up_list = line.split('|l|')[1].split(',')
+            for char_look_up in char_look_up_list:
+                look_up_vector = []
+                for digit_str in char_look_up:
+                    if digit_str == '0' or digit_str == '1':
+                        look_up_vector.append(int(digit_str))
+                matrix.append(look_up_vector)
+            tensor.append(matrix)
+        print(tensor[0])
+        x = np.array(tensor)
+        del tensor
+        print(x.shape)
+        classes = sorted(list(set(labels)))
+        y = np.asarray([classes.index(item) for item in labels])
+        print('Labels', classes)
+
+        # shuffle
+        f.close()
+        return x, y, len(classes)
+
+x, y, num_classes = get_data()
+x = x.reshape(x.shape[0], x.shape[1], x.shape[2], 1)
+
 model_path = './models/7blkup_4classes.h5'
 model = load_model(model_path)
 model.summary()
@@ -16,6 +48,7 @@ print('num of layers', len(model.layers))
 
 # with a Sequential model
 get_3rd_layer_output = K.function([model.layers[0].input],
-                                  [model.layers[3].output])
+                                  [model.layers[13].output])
 
-# layer_output = get_3rd_layer_output([X])[0]
+layer_output = get_3rd_layer_output([x])[0]
+print(layer_output.shape)
--- read_model_7bits.py
+++ read_model_7bits.py
@@ -9,7 +9,7 @@ from sklearn.utils import shuffle
 
 
 def get_data():
-    with open('./datasets/ag_dataset_7bit_20000_look_up.txt', 'r', encoding='utf8') as f:
+    with open('./datasets/ag_7blkup_4_cl_gt_50.txt', 'r', encoding='utf8') as f:
         lines = f.readlines()
         tensor = []
         labels = []
