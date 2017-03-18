e93ba50af96b0c694057d366ce2ce3f5c46662b5
kingy <kingychiu@gmail.com>
2017-03-18 23:40:35 +0800

    knn

--- knn.py
+++ knn.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
 with open('./datasets/7blkup_4classes_dfeatures.txt', 'r', encoding='utf8') as f:
     lines = f.readlines()
     labels = []
@@ -10,4 +13,3 @@ with open('./datasets/7blkup_4classes_dfeatures.txt', 'r', encoding='utf8') as f
 x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 print(x_train.shape)
 print(x_test.shape)
-from sklearn.neighbors import KNeighborsClassifier
