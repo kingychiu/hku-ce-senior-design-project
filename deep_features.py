fc6d64d2af3229f4972870ffbb579eded336f1c8
kingy <kingychiu@gmail.com>
2017-03-19 03:24:09 +0800

    deep features for 5 classes

--- deep_features.py
+++ deep_features.py
@@ -13,7 +13,7 @@ from file_io import FileIO
 
 
 def get_data():
-    with open('./datasets/ag_7blkup_4_cl_gt_50.txt', 'r', encoding='utf8') as f:
+    with open('./datasets/ag_7blkup_5_cl_gt_50.txt', 'r', encoding='utf8') as f:
         lines = f.readlines()
         tensor = []
         labels = []
@@ -63,9 +63,6 @@ while len(x) != 0 and len(lines) < 100000:
         output = ["%.4f" % item for item in intermediate_output[i].tolist()]
         f = ','.join(output)
         lines.append(batch_y[i] + '|sep|' + f)
-FileIO.write_lines_to_file('./datasets/7blkup_4classes_dfeatures.txt', lines)
+FileIO.write_lines_to_file('./datasets/7blkup_5classes_dfeatures.txt', lines)
 
 
-#
-# kmeans = KMeans(n_clusters=10, random_state=0).fit(intermediate_output)
-# print(kmeans.labels_[:10])
