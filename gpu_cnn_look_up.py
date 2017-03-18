c91aa7da2b77ccea93b1c2a2c9b29ec88ddf076f
kingy <kingychiu@gmail.com>
2017-03-18 12:08:48 +0800

    lookup for 4 classes

--- gpu_cnn_look_up.py
+++ gpu_cnn_look_up.py
@@ -17,7 +17,7 @@ import datetime
 
 
 def get_data():
-    with open('./datasets/ag_dataset_7bit_look_up.txt', 'r', encoding='utf8') as f:
+    with open('./datasets/ag_dataset_7bit_20000_look_up_4_classes.txt', 'r', encoding='utf8') as f:
         lines = f.readlines()
         tensor = []
         labels = []
