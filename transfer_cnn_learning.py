285187d623f080ade3f77be1555cc65482516578
kingy <kingychiu@gmail.com>
2017-03-21 00:35:38 +0800

    transfer learning

---
+++ transfer_cnn_learning.py
@@ -0,0 +1,14 @@
+# numpy
+import numpy as np
+# keras
+from keras.utils import np_utils
+from keras.models import load_model
+# sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+model_path = './models/7blkup_4classes.h5'
+model = load_model(model_path)
+print('Read Model Done')
+print(len(model.layers))
+# remove the classification layers
