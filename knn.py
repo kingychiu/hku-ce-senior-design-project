65f9a71ef6efde4ff11a3dfeff3cf8e1679886c2
kingy <kingychiu@gmail.com>
2017-03-21 01:03:23 +0800

    knn

--- knn.py
+++ knn.py
@@ -2,7 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
 
-with open('./datasets/7blkup_2_dfeatures.txt', 'r', encoding='utf8') as f:
+with open('./datasets/7blkup_dfeatures.txt', 'r', encoding='utf8') as f:
     lines = f.readlines()
     labels = []
     features = []
@@ -13,11 +13,11 @@ with open('./datasets/7blkup_2_dfeatures.txt', 'r', encoding='utf8') as f:
     print(len(features[0]))
     f.close()
 
-# x_train = features[:35000]
-# x_test = features[35000:35050]
-# y_train = labels[:35000]
-# y_test = labels[35000:35050]
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+x_train = features[:35000]
+x_test = features[35000:50000]
+y_train = labels[:35000]
+y_test = labels[35000:50000]
+# x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 classes = sorted(list(set(y_train)))
 print(classes)
 print('train', len(x_train))
