d4e547069a4f1249032cec63da063055aa32b315
kingy <kingychiu@gmail.com>
2017-03-31 15:26:17 +0800

    knn

--- knn.py
+++ knn.py
@@ -2,60 +2,76 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans
 
+stat = {}
+
 with open('./datasets/switch_ag12bbc.txt', 'r', encoding='utf8') as f:
     lines = f.readlines()
     print(len(lines))
     labels = []
     features = []
     for line in lines:
-        labels.append(line.split('|sep|')[0])
-        features.append(line.split('|sep|')[1].split(','))
+        label = line.split('|sep|')[0]
+        if label in stat.keys() and stat[label] <= 10000:
+            stat[label] += 1
+            labels.append(label)
+            features.append(line.split('|sep|')[1].split(','))
+        else:
+            stat[label] = 1
+            labels.append(label)
+            features.append(line.split('|sep|')[1].split(','))
+
     print(labels[:10])
     print(len(features[0]))
     f.close()
 
-x_train = features[:50000]
-x_test = features[50000:100000]
-y_train = labels[:20000]
-y_test = labels[50000:100000]
-# x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
-classes = sorted(list(set(y_train)))
-print(classes)
-print('train', len(x_train))
-print('test', len(x_test))
-del labels
-del features
+print(stat)
 
-neigh = KNeighborsClassifier(n_neighbors=100)
-neigh.fit(x_train, y_train)
 
-predictions = []
-count = len(x_test)
-for sample in x_test:
-    predictions.append(neigh.predict(sample))
-    count = count - 1
-    print(count)
-print(predictions[:10])
+# make sure every label
 
-t = 0
-t_by_class = {}
-total_by_class = {}
-for i in range(len(predictions)):
-    p = predictions[i][0]
-    if p in total_by_class.keys():
-        total_by_class[p] += 1
-    else:
-        total_by_class[p] = 1
-    if p == y_test[i]:
-        t += 1
-        if p in t_by_class.keys():
-            t_by_class[p] += 1
-        else:
-            t_by_class[p] = 1
 
-print(t / len(predictions))
-print()
-print('knn train data,', len(x_train))
-print('testing data', len(x_test))
-for k in list(t_by_class.keys()):
-    print(k, t_by_class[k] / total_by_class[k])
+# x_train = features[:50000]
+# x_test = features[50000:100000]
+# y_train = labels[:20000]
+# y_test = labels[50000:100000]
+# # x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+# classes = sorted(list(set(y_train)))
+# print(classes)
+# print('train', len(x_train))
+# print('test', len(x_test))
+# del labels
+# del features
+#
+# neigh = KNeighborsClassifier(n_neighbors=100)
+# neigh.fit(x_train, y_train)
+#
+# predictions = []
+# count = len(x_test)
+# for sample in x_test:
+#     predictions.append(neigh.predict(sample))
+#     count = count - 1
+#     print(count)
+# print(predictions[:10])
+#
+# t = 0
+# t_by_class = {}
+# total_by_class = {}
+# for i in range(len(predictions)):
+#     p = predictions[i][0]
+#     if p in total_by_class.keys():
+#         total_by_class[p] += 1
+#     else:
+#         total_by_class[p] = 1
+#     if p == y_test[i]:
+#         t += 1
+#         if p in t_by_class.keys():
+#             t_by_class[p] += 1
+#         else:
+#             t_by_class[p] = 1
+#
+# print(t / len(predictions))
+# print()
+# print('knn train data,', len(x_train))
+# print('testing data', len(x_test))
+# for k in list(t_by_class.keys()):
+#     print(k, t_by_class[k] / total_by_class[k])
