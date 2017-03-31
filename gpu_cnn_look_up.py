55b713910292f5416f41f9ebfab7b230fefcf770
kingy <kingychiu@gmail.com>
2017-03-31 14:40:52 +0800

    text_emotion

--- gpu_cnn_look_up.py
+++ gpu_cnn_look_up.py
@@ -17,7 +17,7 @@ import datetime
 
 
 def get_data():
-    with open('./datasets/am_review_dataset_7blkup.txt', 'r', encoding='utf8') as f:
+    with open('./datasets/text_emotion_7blkup.txt', 'r', encoding='utf8') as f:
         lines = f.readlines()
         tensor = []
         labels = []
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from preprocess import PreProcess
 
-p = PreProcess('./datasets/amz_review_binary.txt')
+p = PreProcess('./datasets/text-emotion-dataset.txt')
 labels, tensor = p.run_look_up()
 print(tensor[0])
-with open('./datasets/am_review_dataset_7blkup.txt', 'w', encoding='utf8') as f1:
+with open('./datasets/text_emotion_7blkup.txt', 'w', encoding='utf8') as f1:
     document_strs = []
     for matrix in tensor:
         char_strs = []
