1f31fc6ccc955ff0e928d6e833103b5b466663d1
kingy <kingychiu@gmail.com>
2017-04-05 22:40:47 +0800

    word2vec

--- facebook_reader.py
+++ facebook_reader.py
@@ -2,7 +2,8 @@ import requests
 from file_io import FileIO
 base_path = "https://graph.facebook.com"
 token = "1825924290976649|f4a421b77888587f351418a5aa84762c"
-page_ids = ['BillGates', 'elonmuskofficial']
+# page_ids = ['BillGates', 'elonmuskofficial', 'emmawatson', 'barackobama', 'DonaldTrump']
+page_ids = ['emmawatson', 'barackobama', 'DonaldTrump']
 
 for page_id in page_ids:
     texts = []
