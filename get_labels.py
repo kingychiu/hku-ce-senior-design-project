9536dca3941f90412ddf86261c1c0b5e8e5e0be1
kingy <kingychiu@gmail.com>
2017-04-05 21:22:30 +0800

    word2vec

--- get_labels.py
+++ get_labels.py
@@ -51,7 +51,7 @@ intermediate_layer_model.summary()
 
 def get_deep_features(string):
     input = string27Bits(string)
-    input = np.array(input)
+    input = np.array([input])
     # (135386, 100, 7, 1)
     print(input.shape)
     input = input.reshape(input.shape[0], input.shape[1], input.shape[2], 1)
