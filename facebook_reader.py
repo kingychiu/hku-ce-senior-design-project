cab5e9b2bb40343b24c036ae7f3ecbd294b23d9f
kingy <kingychiu@gmail.com>
2017-04-05 22:05:12 +0800

    word2vec

--- facebook_reader.py
+++ facebook_reader.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 base_path = "https://graph.facebook.com"
 token = "1825924290976649|f4a421b77888587f351418a5aa84762c"
 page_ids = ['technologyreview']
@@ -7,4 +7,5 @@ page_ids = ['technologyreview']
 for page_id in page_ids:
     feedRequestUrl = base_path + "/" + page_id + "/feed?access_token=" + token
     r = requests.get(feedRequestUrl)
-    print(r.json())
+    json_dict = data = json.load(r.json())
+    print(json_dict.keys())
