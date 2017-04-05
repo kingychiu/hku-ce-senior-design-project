ea6197e6c82b9eacca0ad7694fdaab5f0cba4d3d
kingy <kingychiu@gmail.com>
2017-04-05 18:26:25 +0800

    word2vec

--- word2vec.py
+++ word2vec.py
@@ -1,38 +1,43 @@
 import gensim
 from gensim.models.keyedvectors import KeyedVectors
 
+print('loading word2vec model...')
 # load google pretrained word2vec model
-model =  KeyedVectors.load_word2vec_format('./word2vec_model/GoogleNews-vectors-negative300.bin', binary=True)
+word_vectors = KeyedVectors.load_word2vec_format(
+    './word2vec_model/GoogleNews-vectors-negative300.bin', binary=True)
 
+print('loading text...')
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+import nltk.data
 
-# from bs4 import BeautifulSoup
-# import re
-# from nltk.corpus import stopwords
-# import nltk.data
-#
-# sentences = []
-# with open('../dataset/all_data_set.txt', 'r', encoding='utf8') as f:
-#     lines = f.readlines()
-#     lines = [line.split('|sep|') for line in lines]
-#     print(len(lines))
-#     # Load the punkt tokenizer
-#     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-#     # English stop words
-#     stops = set(stopwords.words("english"))
-#     for line in lines:
-#         label = line[0]
-#         text = line[1]
-#         # Remove HTML
-#         text = BeautifulSoup(text).get_text()
-#         # Remove non-letters
-#         text = re.sub("[^a-zA-Z]", " ", text)
-#         # Convert words to lower case
-#         words = text.lower().split()
-#         words = [w for w in words if not w in stops]
-#         text = ' '.join(words)
-#         if len(text.split()) <= 3:
-#             continue
-#         new_line = label + '|sep|' + text
-#         sentences.append(new_line)
-#
-#     print(new_line[0])
+sentences = []
+with open('../dataset/all_data_set.txt', 'r', encoding='utf8') as f:
+    lines = f.readlines()
+    lines = [line.split('|sep|') for line in lines]
+    print(len(lines))
+    # Load the punkt tokenizer
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    # English stop words
+    stops = set(stopwords.words("english"))
+    for line in lines:
+        label = line[0]
+        text = line[1]
+        # Remove HTML
+        text = BeautifulSoup(text).get_text()
+        # Remove non-letters
+        text = re.sub("[^a-zA-Z]", " ", text)
+        # Convert words to lower case
+        words = text.lower().split()
+        words = [w for w in words if not w in stops]
+        text = ' '.join(words)
+        if len(text.split()) <= 3:
+            continue
+        for word in text:
+            if text in word_vectors.keys():
+                print(word_vectors[text])
+                break
+        new_line = label + '|sep|' + text
+        break
+        sentences.append(new_line)
