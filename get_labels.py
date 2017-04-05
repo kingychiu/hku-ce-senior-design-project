7f4e1578c4b272450cb373e3d9a0cdbd6dc14084
kingy <kingychiu@gmail.com>
2017-04-05 23:03:59 +0800

    word2vec

--- get_labels.py
+++ get_labels.py
@@ -113,7 +113,7 @@ def get_labels(string, sumary):
 
 
 print('ready')
-with open('./fb_posts/DonaldTrump.txt', 'r', encoding='utf8') as fb_posts:
+with open('./fb_posts/JustinPJTrudeau.txt', 'r', encoding='utf8') as fb_posts:
     summary = {}
     for c in classes:
         summary[c] = 0
