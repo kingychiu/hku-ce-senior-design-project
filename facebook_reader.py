f3432d60145c3c7d0fba35c9e79805a1626ee545
kingy <kingychiu@gmail.com>
2017-04-05 22:08:54 +0800

    word2vec

--- facebook_reader.py
+++ facebook_reader.py
@@ -5,15 +5,16 @@ token = "1825924290976649|f4a421b77888587f351418a5aa84762c"
 page_ids = ['technologyreview']
 
 for page_id in page_ids:
-
-
+    texts = []
     feedRequestUrl = base_path + "/" + page_id + "/feed?access_token=" + token
+
     def do(url):
         r = requests.get(url)
         json_dict = r.json()
-        print(json_dict['data'])
+        for data in json_dict['data']:
+            texts.append(data['message'])
+        print(len(texts))
         if 'paging' in json_dict.keys():
-            pass
-            # do(url)
+            do(url)
 
     do(feedRequestUrl)
