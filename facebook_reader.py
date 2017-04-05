7e93bf896f9a4b056947f8737b4a416f6cd37fbd
kingy <kingychiu@gmail.com>
2017-04-05 22:03:54 +0800

    word2vec

---
+++ facebook_reader.py
@@ -0,0 +1,10 @@
+import requests
+
+base_path = "https://graph.facebook.com"
+token = "1825924290976649|f4a421b77888587f351418a5aa84762c"
+page_ids = ['technologyreview']
+
+for page_id in page_ids:
+    feedRequestUrl = base_path + "/" + page_id + "/feed?access_token=" + token
+    r = requests.get(feedRequestUrl)
+    print(r.json())
