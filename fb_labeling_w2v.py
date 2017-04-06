7eedb5b0cd806e9f6dc10c6c61854a8100146fff
kingy <kingychiu@gmail.com>
2017-04-06 21:22:10 +0800

    word2vec

--- fb_labeling_w2v.py
+++ fb_labeling_w2v.py
@@ -1,10 +1,6 @@
 # numpy
 import numpy as np
-# keras
-from keras.utils import np_utils
-from keras.models import load_model
-from keras.models import Model
-from keras import backend as K
+
 # sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
