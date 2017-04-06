431edfa619736657e555718fade20a2ff6c37caa
kingy <kingychiu@gmail.com>
2017-04-06 20:35:10 +0800

    word2vec

--- word2vec_simiar_words.py
+++ word2vec_simiar_words.py
@@ -19,42 +19,29 @@ import operator, functools
 
 labels = []
 related_words = []
-with open('./datasets/all_data_set.txt', 'r', encoding='utf8') as f:
+with open('./datasets/word2vec_ag12bbc.txt', 'r', encoding='utf8') as f:
     lines = f.readlines()
     lines = [line.split('|sep|') for line in lines]
     print(len(lines))
-    # Load the punkt tokenizer
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    # English stop words
-    stops = set(stopwords.words("english"))
-
     for line in lines:
         label = line[0]
-        text = line[1]
-        # Remove HTML
-        text = BeautifulSoup(text).get_text()
-        # Remove non-letters
-        text = re.sub("[^a-zA-Z]", " ", text)
-        # Convert words to lower case
-        words = text.lower().split()
-        words = [w for w in words if not w in stops]
-        if len(words) <= 3:
-            continue
+        vector = line[1]
         bag_of_words = {}
-        for word in words:
-            # get similar word of the word
-            try:
-                simiar_words = word_vectors.similar_by_word(word, topn=10)
-                for w in simiar_words:
-                    if w[0] in bag_of_words.keys():
-                        bag_of_words[w[0]] += w[1]
-                    else:
-                        bag_of_words[w[0]] = w[1]
-            except:
-                pass
+        # get similar word of the word
+        try:
+            simiar_words = word_vectors.similar_by_vector(vector, topn=10)
+            for w in simiar_words:
+                if w[0] in bag_of_words.keys():
+                    bag_of_words[w[0]] += w[1]
+                else:
+                    bag_of_words[w[0]] = w[1]
+        except:
+            pass
+
         print(bag_of_words)
         related_words.append(sorted(bag_of_words.items(), key=operator.itemgetter(1), reverse=True)[10:])
         print(len(related_words))
+        break
         # print(text)
         # for s in sorted(bag_of_words.items(), key=operator.itemgetter(1), reverse=True):
         #     if s[1] != 0:
