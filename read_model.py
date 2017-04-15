16b658eba445f8553d12c188f32b4bcad8a34fe5
kingy <kingychiu@gmail.com>
2017-04-15 14:21:27 +0800

    read model

--- read_model.py
+++ read_model.py
@@ -25,9 +25,8 @@ def get_data():
                 for digit_str in char_look_up:
                     if digit_str == '0' or digit_str == '1':
                         look_up_vector.append(int(digit_str))
-                if len(look_up_vector) != 7:
-                    continue
-                matrix.append(look_up_vector)
+                if len(look_up_vector) == 7:
+                    matrix.append(look_up_vector)
             tensor.append(matrix)
         print(tensor[0])
         x = np.array(tensor)
